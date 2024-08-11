03c65aeb414b02a6b71488651855af5d3fb94b65
hugornda <hugornda@gmail.com>
2024-08-11 23:05:01 +0100

    add first step of lambda function

---
+++ cmd/picapuento/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Request struct {
+	Action string `json:"action"`
+}
+
+func HandleRequest(ctx context.Context, event *Request) (*string, error) {
+	if event == nil {
+		return nil, fmt.Errorf("received nil event")
+	}
+
+	message := fmt.Sprintf("Action pretended is %s!", event.Action)
+	return &message, nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
